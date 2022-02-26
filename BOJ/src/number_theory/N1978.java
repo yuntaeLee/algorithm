6cbd9923e0de21ba8ab3de86d9dc9b2e9ac3e568
yuntaeLee <leeyt1201@gmail.com>
2022-02-26 15:57:05 +0900

    BOJ

--- BOJ/src/number_theory/N1978.java
+++ BOJ/src/number_theory/N1978.java
@@ -1,9 +1,36 @@
 package number_theory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class N1978 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		boolean[] decimal = new boolean[1001];
+		decimal[0] = decimal[1] = true;
+		
+		for (int i = 2; i < Math.sqrt(decimal.length); i++) {
+			for (int j = i * i; j <= 1000; j = j + i) {
+				if (!decimal[j])
+					 decimal[j] = true;
+			}
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			if (!decimal[Integer.parseInt(st.nextToken())])
+				 count++;
+		}
+		
+		System.out.println(count);
 	}
 
 }
