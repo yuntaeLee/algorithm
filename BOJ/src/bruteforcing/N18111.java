bb3d9b8eba77f3f9dc85730e434913b583b2938c
yuntaeLee <leeyt1201@gmail.com>
2022-03-05 02:45:20 +0900

    Minecraft

---
+++ BOJ/src/bruteforcing/N18111.java
@@ -0,0 +1,75 @@
+package bruteforcing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N18111 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		int[][] ground = new int[N][M];
+		int min = 256;
+		int max = 0;
+		
+		int answerTime = Integer.MAX_VALUE;
+		int answerHeight = 0;
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < M; j++) {
+				ground[i][j] = Integer.parseInt(st.nextToken());
+				
+				min = Math.min(min, ground[i][j]);
+				max = Math.max(max, ground[i][j]);
+			}
+		}
+		
+		for (int height = min; height <= max; height++) {
+			
+			int time = 0;
+			int block = B;
+			
+			for (int n = 0; n < N; n++) {
+				for (int m = 0; m < M; m++) {
+					
+					if (ground[n][m] == height) continue;
+					
+					else {
+						int diff = Math.abs(height - ground[n][m]);
+						if (height > ground[n][m]) {
+							block -= diff;
+							time += diff;
+						}
+						
+						else {
+							block += diff;
+							time += 2 * diff;
+						}
+					}
+				}
+			}
+			
+			if (block >= 0) {
+				// 시간이 같더라도 땅이 높이가 높은 것이 우선이기 때문에 부등호 <= 사용
+				if (time <= answerTime) {
+					answerTime = time;
+					answerHeight = height;
+				}
+			}
+		}
+		
+		sb.append(answerTime).append(' ').append(answerHeight);
+		System.out.println(sb);
+	}
+}
