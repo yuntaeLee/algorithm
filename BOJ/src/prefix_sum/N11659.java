a9f47f52631959b15781d071aa4904d22fc3f2eb
yuntaeLee <leeyt1201@gmail.com>
2022-03-08 22:52:06 +0900

    Prefix sum 4

---
+++ BOJ/src/prefix_sum/N11659.java
@@ -0,0 +1,36 @@
+package prefix_sum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N11659 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N + 1];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+		}
+		
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			sb.append(arr[end] - arr[start - 1]).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+
+}
