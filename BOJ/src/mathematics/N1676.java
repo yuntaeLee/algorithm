2414d71ca75276196d60bc09d85cdd899d00fa08
yuntaeLee <leeyt1201@gmail.com>
2022-03-08 20:53:18 +0900

    Number of factorial zeros

---
+++ BOJ/src/mathematics/N1676.java
@@ -0,0 +1,22 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N1676 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+
+		while (N >= 5) {
+			count += N / 5;
+			N /= 5;
+		}
+		
+		System.out.println(count);
+	}
+}
