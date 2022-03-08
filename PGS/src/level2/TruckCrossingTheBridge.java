5f572195fd690decaca4dd2ec17d9e09cac6604d
yuntaeLee <leeyt1201@gmail.com>
2022-03-09 07:30:42 +0900

    TruckCrossingTheBridge: Level 2

---
+++ PGS/src/level2/TruckCrossingTheBridge.java
@@ -0,0 +1,53 @@
+package level2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TruckCrossingTheBrdge {
+	
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
+		int answer = 0;
+		int sum = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < truck_weights.length; i++) {
+
+        	int truck = truck_weights[i];
+
+        	while (true) {
+        		if (q.isEmpty()) {
+	                q.add(truck);
+	                sum += truck;
+	                answer++;
+	                break;
+                }
+
+                else if (q.size() == bridge_length) {
+                    sum -= q.poll();
+                }
+
+                else {
+
+                    if (sum + truck <= weight) {
+                        q.add(truck);
+                        sum += truck;
+                        answer++;
+                        break;
+                    }
+                    else {
+                        q.add(0);
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer + bridge_length;
+   }
+	   
+	public static void main(String[] args) {
+		
+	}
+
+}
