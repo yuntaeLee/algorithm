35644c4776aba6e6473285eabe2ff74d86ef814c
yuntaeLee <leeyt1201@gmail.com>
2022-03-08 20:57:28 +0900

    Find password: HashMap

---
+++ BOJ/src/data_structures/N17219.java
@@ -0,0 +1,32 @@
+package data_structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class N17219 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			map.put(st.nextToken(), st.nextToken());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			sb.append(map.get(br.readLine())).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
