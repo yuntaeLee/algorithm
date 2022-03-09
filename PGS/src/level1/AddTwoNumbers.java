560bd00a73fcb27da30b8f13dd7d8234245661ef
yuntaeLee <leeyt1201@gmail.com>
2022-03-10 07:51:45 +0900

    TreeSet

---
+++ PGS/src/level1/AddTwoNumbers.java
@@ -0,0 +1,32 @@
+package level1;
+
+import java.util.TreeSet;
+
+public class AddTwoNumbers {
+	
+	public int[] solution(int[] numbers) {
+        int[] answer = {};
+        
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        answer = new int[set.size()];
+        int idx = 0;
+        
+        for (int e : set) {
+            answer[idx++] = e;
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		
+	}
+
+}
