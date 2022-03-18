0ee8c557a59b390dd5e6b76ce58d2cc7629a5960
yuntaeLee <leeyt1201@gmail.com>
2022-03-18 14:45:51 +0900

    Reparman: review

---
+++ BOJ/src/greedy/N1449.java
@@ -0,0 +1,38 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N1449 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int count = 1;
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		double start = arr[0] - 0.5;
+		for (int i = 0; i < N; i++) {
+			if (start + L < arr[i]) {
+				count++;
+				start = arr[i] - 0.5;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
