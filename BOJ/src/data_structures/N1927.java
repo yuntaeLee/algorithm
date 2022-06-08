435f2fe484e9cbb801edac721993a547fd3a19ff
yuntaeLee <leeyt1201@gmail.com>
2022-06-08 15:30:09 +0900

    Min heap: PriorityQueue

---
+++ BOJ/src/data_structures/N1927.java
@@ -0,0 +1,30 @@
+package data_structures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class N1927 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		while (N-- > 0) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if (x == 0) {
+				if (pq.isEmpty()) sb.append(0).append('\n');
+				else sb.append(pq.poll()).append('\n');
+			}
+			
+			else pq.offer(x);
+		}
+		
+		System.out.println(sb);
+	}
+}
