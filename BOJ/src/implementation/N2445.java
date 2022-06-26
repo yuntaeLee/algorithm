9c8d728952d1935dfbe21af684db3c4fcf85ce84
yuntaeLee <leeyt1201@gmail.com>
2022-06-26 14:06:49 +0900

    Star 08

---
+++ BOJ/src/implementation/N2445.java
@@ -0,0 +1,50 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N2445 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				sb.append('*');
+			}
+			
+			for (int k = 1; k <= 2 * (n - i); k++) {
+				sb.append(' ');
+			}
+			
+			for (int j = 1; j <= i; j++) {
+				sb.append('*');
+			}
+			
+			sb.append('\n');
+		}
+		
+		for (int i = n - 1; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				sb.append('*');
+			}
+			
+			for (int k = 1; k <= 2 * (n - i); k++) {
+				sb.append(' ');
+			}
+			
+			for (int j = 1; j <= i; j++) {
+				sb.append('*');
+			}
+			
+			sb.append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+
+}
