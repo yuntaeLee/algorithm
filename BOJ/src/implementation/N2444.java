56e94547f3fa8eca256639f3f98c5dd719919393
yuntaeLee <leeyt1201@gmail.com>
2022-07-09 20:59:30 +0900

    Star-7

--- BOJ/src/implementation/N2444.java
+++ BOJ/src/implementation/N2444.java
@@ -13,26 +13,17 @@ public class N2444 {
 		int N = Integer.parseInt(br.readLine());
 		
 		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j < N - i; j++) 
-				sb.append(' ');
-			
-			for (int j = 0; j < i * 2 - 1; j++)
-				sb.append('*');
-			
+			for (int j = 0; j < N - i; j++) sb.append(' ');
+			for (int j = 0; j < i * 2 - 1; j++) sb.append('*');
 			sb.append('\n');
 		}
 		
 		for (int i = N - 1; i >= 0; i--) {
-			for (int j = 0; j < N - i; j++)
-				sb.append(' ');
-			
-			for (int j = 0; j < i * 2 - 1; j++)
-				sb.append('*');
-			
+			for (int j = 0; j < N - i; j++) sb.append(' ');
+			for (int j = 0; j < i * 2 - 1; j++) sb.append('*');
 			sb.append('\n');
 		}
 		
 		System.out.println(sb);
 	}
-
 }
