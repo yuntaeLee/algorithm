0caf12d06ec8170f169e0a2f3675ebd706fe772f
yuntaeLee <leeyt1201@gmail.com>
2022-07-20 12:53:01 +0900

    N and M-8

---
+++ BOJ/src/backtracking/N15657.java
@@ -0,0 +1,54 @@
+package backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N15657 {
+	
+	static int N, M;
+	static int[] arr;
+	static int[] result;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		result = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		backTracking(0, 0, sb);
+		
+		System.out.println(sb);
+	}
+	
+	static void backTracking(int start, int cnt, StringBuilder sb) {
+		
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(result[i]).append(' ');
+			}
+			sb.append('\n');
+		}
+		
+		else {
+			for (int i = start; i < N; i++) {
+				result[cnt] = arr[i];
+				backTracking(i, cnt + 1, sb);
+			}
+		}
+	}
+}
