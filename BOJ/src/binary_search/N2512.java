a069908ed5c50740e5111bb33f4e84c0f71fe356
yuntaeLee <leeyt1201@gmail.com>
2022-09-14 20:03:50 +0900

    Budget

---
+++ BOJ/src/binary_search/N2512.java
@@ -0,0 +1,43 @@
+package binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N2512 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		int left = 0;
+		int right = -1;
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			right = Math.max(right, arr[i]);
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			long budget = 0;
+			
+			for (int i = 0; i < N; i++) {
+				if (arr[i] > mid) budget += mid;
+				else budget += arr[i];
+			}
+			
+			if (budget <= M) left = mid + 1;
+			else right = mid - 1;
+		}
+		
+		System.out.println(right);
+	}
+}
