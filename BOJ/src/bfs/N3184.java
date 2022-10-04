f6475966a4e6ee722677e098feafe5ab69ef5e69
yuntaeLee <leeyt1201@gmail.com>
2022-10-04 10:40:37 +0900

    Sheep

---
+++ BOJ/src/bfs/N3184.java
@@ -0,0 +1,91 @@
+package bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class N3184 {
+	
+	static class Node {
+		int x;
+		int y;
+		
+		Node (int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static int sheep, wolf;
+	static int R, C;
+	static char[][] map;
+	static boolean[][] visited;
+	
+	static int[] dx = {-1, 0, 1, 0};
+	static int[] dy = {0, -1, 0, 1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		visited = new boolean[R][C];
+		
+		for (int i = 0; i < R; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = s.charAt(j);
+			}
+		}
+		
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if (!visited[i][j]) {
+					bfs(i, j);
+				}
+			}
+		}
+		
+		System.out.println(sheep + " " + wolf);
+	}
+	
+	static void bfs(int x, int y) {
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(x, y));
+		visited[x][y] = true;
+		
+		int s = 0;
+		int w = 0;
+		
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				
+				if (outOfArea(nx, ny)) continue;
+				
+				if (!visited[nx][ny] && map[nx][ny] != '#') {
+					visited[nx][ny] = true;
+					q.offer(new Node(nx, ny));
+					
+					if (map[nx][ny] == 'o') s++;
+					else if (map[nx][ny] == 'v') w++;
+				}
+			}
+		}
+		
+		if (s > w) sheep += s;
+		else wolf += w;
+	}
+	
+	static boolean outOfArea(int x, int y) {
+		return x < 0 || y < 0 || x >= R || y >= C;
+	}
+}
