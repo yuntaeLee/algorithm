358112ae3e30fe72626e3c798bf398fa68098f67
yuntaeLee <leeyt1201@gmail.com>
2022-10-09 21:59:22 +0900

    Sequence

---
+++ BOJ/src/prefix_sum/N2559.java
@@ -0,0 +1,38 @@
+package prefix_sum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N2559 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int ans = Integer.MIN_VALUE;
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i <= N - K; i++) {
+			int sum = 0;
+			
+			for (int j = i; j < i + K; j++) {
+				sum += arr[j];
+			}
+			
+			ans = Math.max(ans, sum);
+		}
+		
+		System.out.println(ans);
+	}
+
+}
