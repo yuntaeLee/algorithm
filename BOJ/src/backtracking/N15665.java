2ccf742cf1086ef8f54d16cfcd1bd88277ed357e
yuntaeLee <leeyt1201@gmail.com>
2022-10-22 22:13:30 +0900

    N and M 11 **

--- BOJ/src/backtracking/N15665.java
+++ BOJ/src/backtracking/N15665.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class N15665 {
