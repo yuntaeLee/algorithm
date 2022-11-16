7d43d5ad316c9eb48399472cd906d1e7efeded68
yuntaeLee <leeyt1201@gmail.com>
2022-11-16 15:21:12 +0900

    Network recovery: get Count and Path

---
+++ BOJ/src/mathematics/N3733.java
@@ -0,0 +1,26 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N3733 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		String s;
+		while ((s = br.readLine()) != null) {
+			st = new StringTokenizer(s);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			sb.append(b / (a + 1)).append('\n');
+		}
+			
+		System.out.println(sb);
+	}
+}
