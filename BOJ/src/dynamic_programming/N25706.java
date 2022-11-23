80240d2d2ff37177545d3e69845a544baa01b7a4
yuntaeLee <leeyt1201@gmail.com>
2022-11-23 15:51:42 +0900

    Bike Stunts

---
+++ BOJ/src/dynamic_programming/N25706.java
@@ -0,0 +1,40 @@
+package dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N25706 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[N - 1] = 1;
+		for (int i = N - 2; i >= 0; i--) {
+			if (arr[i] + 1 + i < N) {
+				dp[i] = dp[arr[i] + 1 + i] + 1;
+				continue;
+			}
+			
+			dp[i] = 1;
+		}
+		 
+		for (int val : dp) {
+			sb.append(val).append(' ');
+		}
+		
+		System.out.println(sb);
+	}
+}
