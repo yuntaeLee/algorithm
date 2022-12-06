fb4a3a15cfd02393a48615f7b8fe592fdaee746f
yuntaeLee <leeyt1201@gmail.com>
2022-12-06 15:21:14 +0900

    단어 변환

---
+++ PGS/src/level0/Factorization.java
@@ -0,0 +1,28 @@
+package level0;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class Factorization {
+
+	public static void main(String[] args) {
+		solution(420);
+	}
+
+	static int[] solution(int n) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        
+        for (int i = 2; i < Math.sqrt(n) + 1; i++) {
+            while (n % i == 0) {
+                n /= i;
+                set.add(i);
+            }
+        }
+        
+        if (n != 1) {
+            set.add(n);
+        }
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
