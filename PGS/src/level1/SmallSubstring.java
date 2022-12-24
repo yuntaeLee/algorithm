6f896eb7b2d3df728b0f8422a80a7351e17d3bae
yuntaeLee <leeyt1201@gmail.com>
2022-12-24 23:12:58 +0900

    크기가 작은 부분문자열

--- PGS/src/level1/SmallSubstring.java
+++ PGS/src/level1/SmallSubstring.java
@@ -3,8 +3,21 @@ package level1;
 public class SmallSubstring {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(solution("500220839878", "7"));
 	}
-
+	
+    static int solution(String t, String p) {
+        int answer = 0;
+        for (int i = 0; i < t.length() - p.length() + 1; i++) {
+            if (isCorrect(t.substring(i, i + p.length()), p)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    static boolean isCorrect(String t, String p) {
+        return Long.parseLong(t) <= Long.parseLong(p);
+    }
 }
