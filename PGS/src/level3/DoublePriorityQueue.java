95b50e018c59b06d9bf8f95e4ab5d537f4c10d8d
yuntaeLee <leeyt1201@gmail.com>
2022-12-25 11:14:17 +0900

    이중 우선순위 큐

--- PGS/src/level3/DoublePriorityQueue.java
+++ PGS/src/level3/DoublePriorityQueue.java
@@ -42,4 +42,5 @@ public class DoublePriorityQueue {
         
         return answer;
     }
+	
 }
---
+++ PGS/src/level3/IntegerTriangle.java
@@ -0,0 +1,30 @@
+package level3;
+
+public class IntegerTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    static int solution(int[][] triangle) {
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+            }
+        }
+        
+        int answer = triangle[0][0];
+        for (int i = 0; i < triangle.length; i++) {
+            for (int j = 0; j < i; j++) {
+                answer = Math.max(answer, triangle[i][j]);
+            }
+        }
+        
+        return answer;
+    }
+
+}
