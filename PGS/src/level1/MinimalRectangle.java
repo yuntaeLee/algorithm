0ee3450384f621da828c08c814f456fefb51ed4c
yuntaeLee <leeyt1201@gmail.com>
2023-01-25 16:42:26 +0900

    짝지어 제거하기: Stack

---
+++ PGS/src/level1/MinimalRectangle.java
@@ -0,0 +1,19 @@
+package level1;
+
+public class MinimalRectangle {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    static int solution(int[][] sizes) {
+        int w = 0;
+        int h = 0;
+        for (int[] size : sizes) {
+            w = Math.max(w, Math.max(size[0], size[1]));
+            h = Math.max(h, Math.min(size[0], size[1]));
+        }
+        
+        return w * h;
+    }
+}
