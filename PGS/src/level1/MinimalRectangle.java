bde26861afa4fd4781b30352d4ce5da6bd239fa8
yuntaeLee <leeyt1201@gmail.com>
2023-01-27 12:30:58 +0900

    최소직사각형

--- PGS/src/level1/MinimalRectangle.java
+++ PGS/src/level1/MinimalRectangle.java
@@ -3,7 +3,16 @@ package level1;
 public class MinimalRectangle {
 
 	public static void main(String[] args) {
+		int[][] input = {
+				{10, 7},
+				{12, 3},
+				{8, 15},
+				{14, 7},
+				{5, 15}};
 		
+		int result = 120;
+		
+		System.out.println(result == solution(input));
 	}
 	
     static int solution(int[][] sizes) {
