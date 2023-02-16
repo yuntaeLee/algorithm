674773080b2f99231636afc743e36934d7a58d38
yuntaeLee <leeyt1201@gmail.com>
2023-02-16 13:21:40 +0900

    점프와 순간 이동

--- PGS/src/level2/JumpAndTeleport.java
+++ PGS/src/level2/JumpAndTeleport.java
@@ -2,9 +2,12 @@ package level2;
 
 public class JumpAndTeleport {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int input = 5000;
+        int result = 5;
 		
-	}
+        System.out.println(solution(input) == result);
+    }
 	
     static int solution(int n) {
         int ans = 0;
