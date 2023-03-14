ba1dee54299f34d01c91d928de2b6e565d3aa437
yuntaeLee <leeyt1201@gmail.com>
2023-03-14 14:21:27 +0900

    문자열 내 마음대로 정렬하기: Sort

---
+++ PGS/src/level1/SecretMap.java
@@ -0,0 +1,26 @@
+package level1;
+
+public class SecretMap {
+
+	public static void main(String[] args) {
+		int[] arr1 = {9, 20, 28, 18, 11};
+		int[] arr2 = {30, 1, 21, 17, 28};
+		int n = 5;
+		
+		solution(5, arr1, arr2);
+	}
+
+    static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            
+            answer[i] = String.format("%" + n + "s", answer[i]);
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
+        }
+        
+        return answer;
+    }
+}
