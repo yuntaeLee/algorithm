6266729dc419f281d66d2dcabaf2752af4a17eab
yoon <leeyt1201@gmail.com>
2023-07-14 21:24:52 +0900

    접두사

---
+++ BOJ/src/greedy/N1105.java
@@ -0,0 +1,28 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N1105 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String L = st.nextToken();
+        String R = st.nextToken();
+        int count = 0;
+        
+        if (L.length() == R.length()) {
+            for (int i = 0; i < L.length(); i++) {
+                if (L.charAt(i) != R.charAt(i)) break;
+
+                if (L.charAt(i) == '8') count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
