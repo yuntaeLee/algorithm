37dc1b060843193727ac0f4ea5cc17f26774a7ce
yoon <leeyt1201@gmail.com>
2023-08-06 22:33:20 +0900

    문자열 묶기: HashMap

--- PGS/src/level0/StringTying.java
+++ PGS/src/level0/StringTying.java
@@ -5,6 +5,15 @@ import java.util.Map;
 
 public class StringTying {
 
+    public static void main(String[] args) {
+        String[] strArr = {"a","bc","d","efg","hi"};
+
+        int actual = solution(strArr);
+
+        int expected = 2;
+        System.out.println(actual == expected);
+    }
+
     static int solution(String[] strArr) {
         Map<Integer, Integer> map = new HashMap<>();
         for (String s : strArr) {
