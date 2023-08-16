99d10651f8ca1c02b748c4e3441424419276eb57
yoon <leeyt1201@gmail.com>
2023-08-16 18:31:34 +0900

    둘만의 암호

--- PGS/src/level1/TwoPasswords.java
+++ PGS/src/level1/TwoPasswords.java
@@ -1,9 +1,19 @@
 package level1;
 
 public class TwoPasswords {
-    
 
-    public String solution(String s, String skip, int index) {
+    public static void main(String[] args) {
+        String s = "aukks";
+        String skip = "wbqd";
+        int index = 5;
+
+        String actual = solution(s, skip, index);
+
+        String expected = "happy";
+        System.out.println(actual.equals(expected));
+    }
+
+    static String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
         
         for (char letter : s.toCharArray()) {
