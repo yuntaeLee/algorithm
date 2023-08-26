3b4e360570488a073794393516c02b647cde5616
yoon <leeyt1201@gmail.com>
2023-08-26 16:42:11 +0900

    [3차]압축: HashMap

---
+++ PGS/src/level2/Compression.java
@@ -0,0 +1,47 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TertiaryCompression {
+
+    public static void main(String[] args) {
+        String msg = "TOBEORNOTTOBEORTOBEORNOT";
+
+        List<Integer> actual = solution(msg);
+
+        List<Integer> expected = List.of(20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34);
+        System.out.println(actual.equals(expected));
+    }
+    
+    static List<Integer> solution(String msg) {
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < 26; i++) {
+            map.put(String.valueOf((char)('A' + i)), i + 1);
+        }
+        
+        int number = 27;
+        for (int i = 0; i < msg.length(); i++) {
+            StringBuilder temp = new StringBuilder();
+            int index = 0;
+            for (int j = i; j < msg.length(); j++) {
+                temp.append(msg.charAt(j));
+                if (!map.containsKey(temp.toString())) {
+                    break;
+                }
+                
+                index = map.get(temp.toString());
+                i = j;
+            }
+            
+            answer.add(index);
+            map.put(temp.toString(), number++);
+        }
+        
+        return answer;
+    }
+}
