5e77b06a23e14bc7b9d6c0440077a8ec311dc0ce
yoon <leeyt1201@gmail.com>
2023-11-05 20:11:25 +0900

    삼각형 만들기

---
+++ BOJ/src/greedy/N1448.java
@@ -0,0 +1,31 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class N1448 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 3; i >= 0; i--) {
+            if (arr[i] + arr[i + 1] > arr[i + 2]) {
+                System.out.println(arr[i] + arr[i+ 1] + arr[i + 2]);
+                return;
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
