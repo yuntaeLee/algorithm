709154517a20df37bb3e6c28d4ef00a0b86842bb
yoon <leeyt1201@gmail.com>
2023-12-03 12:11:32 +0900

    에너지 드링크

---
+++ BOJ/src/greedy2/N20115.java
@@ -0,0 +1,30 @@
+package greedy2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N20115 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int max = 0;
+        double sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int v = Integer.parseInt(st.nextToken());
+            if (max < v) {
+                max = v;
+            }
+
+            sum += (double) v;
+        }
+
+        System.out.println(max + ((sum - max) / 2));
+    }
+}
