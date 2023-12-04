3acc4105c872119b9d02ac758b8ab64c285d9a8e
yoon <leeyt1201@gmail.com>
2023-12-04 12:10:21 +0900

    라디오

---
+++ BOJ/src/greedy2/N3135.java
@@ -0,0 +1,26 @@
+package greedy2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N3135 {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int min = Math.abs(B - A);
+
+        for (int i = 0; i < N; i++) {
+            int v = Integer.parseInt(br.readLine());
+            min = Math.min(min, Math.abs(B - v) + 1);
+        }
+
+        System.out.println(min);
+    }
+}
