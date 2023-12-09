1521b0311a1ce8e86029bf360c98a2dfb015b868
yoon <leeyt1201@gmail.com>
2023-12-09 18:42:33 +0900

    농장 관리

--- BOJ/src/dfs/N1245.java
+++ BOJ/src/dfs/N1245.java
@@ -3,8 +3,6 @@ package dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class N1245 {
