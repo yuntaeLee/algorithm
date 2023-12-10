d28081ddc867b95767569caecee46849bcca13b5
yoon <leeyt1201@gmail.com>
2023-12-10 20:25:45 +0900

    부등호

--- BOJ/src/backtracking/N2529.java
+++ BOJ/src/backtracking/N2529.java
@@ -3,9 +3,6 @@ package backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class N2529 {
